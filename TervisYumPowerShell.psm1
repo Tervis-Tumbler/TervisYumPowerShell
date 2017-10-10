53441072c6245ae5b13b2f2728e850a30d609429
ChrisMagnuson <hourglasssand@gmail.com>
2017-10-10 17:18:58 -0400

    Initial commit

---
+++ TervisYumPowerShell.psm1
@@ -0,0 +1,82 @@
+﻿$TervisPackageGroups = [PSCustomObject][Ordered] @{
+    Name = "SFTP"
+    PackageInstallDefinition = @"
+puppet
+realmd
+sssd
+oddjob
+oddjob-mkhomedir
+adcli
+samba-common
+"@ -split "`r`n" | New-YumPackageInstallDefinition
+},
+[PSCustomObject][Ordered] @{
+    Name = "Windows Domain Join"
+    PackageInstallDefinition = @"
+krb5-workstation
+realmd
+sssd
+samba-common-tools
+adcli
+"@ -split "`r`n" | New-YumPackageInstallDefinition
+},
+[PSCustomObject][Ordered] @{
+    Name = "ZeroTierBridge"
+    PackageInstallDefinition = @"
+"@ -split "`r`n" | New-YumPackageInstallDefinition
+    PackageGroupToImport = "Windows Domain Join"
+}
+
+function Get-TervisYumPackageGroup {
+    param (
+        $Name
+    )
+    $TervisPackageGroups | where Name -EQ $Name
+}
+
+function New-YumPackageInstallDefinition {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]$Name,
+        $Version
+    )
+    process {
+        [PSCustomObject]@{
+            Name = $Name
+            Version = $Version
+        }
+    }
+}
+
+function New-YUMPackageInstallCommand {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]$PackageInstallDefinition
+    )
+    begin { $PackageInstallDefinitions = @() }
+    process {
+        $PackageInstallDefinitions += $PackageInstallDefinition
+    }
+    end {
+        $YumArguements = (
+            $PackageInstallDefinitions |
+            ForEach-Object { "$($_.Name)-$($_.Version)" }
+        ) -join " "
+
+        "yes | yum -y install $YumArguements"
+    }
+}
+
+function Install-YumTervisPackageGroup {
+    param (
+        [Parameter(Mandatory)]$TervisPackageGroupName,
+        [Parameter(Mandatory,ParameterSetName="SSHSession")]$SSHSession
+    )
+    process {
+        $TervisPackageGroup = Get-TervisYumPackageGroup -Name $TervisPackageGroupName
+        $Command = $TervisPackageGroup.PackageInstallDefinition | New-YUMPackageInstallCommand
+        Invoke-SSHCommand -Command $Command -SSHSession $SSHSession
+    }
+}
+
+function Invoke-YumUpdate {
+    "yum update -y"
+}
